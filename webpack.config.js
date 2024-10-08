9d459bf41942230ef935c558de89f6d83de8bcee
Ilya <sange0337@gmail.com>
2024-10-08 22:38:09 +0800

    finished the font setup

---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: './src/ts/index.ts',
+    styles: ['./src/scss/style.scss', './src/fonts/fonts.css'],
+  },
+  output: {
+    filename: 'js/[name].[contenthash].js',
+    path: path.resolve(__dirname, 'docs'),
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.scss'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.scss$/,  // Add SCSS loader configuration
+        use: [
+          MiniCssExtractPlugin.loader,  // Extract CSS into separate file
+          'css-loader',  // Translates CSS into CommonJS
+          'postcss-loader',  // Optional, for autoprefixing or additional plugins
+          'sass-loader',  // Compiles SCSS to CSS
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/assets/images', to: 'assets/images' },
+        { from: 'src/assets/svgs', to: 'assets/svgs' },
+      ],
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin(),
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    hot: true,
+    watchFiles: ['./src/**/*'],
+    open: false,
+    port: 3000,
+  },
+};
