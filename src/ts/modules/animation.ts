1ee259cc833450f2d5cfb5d2c895151105466f5e
Ilya <sange0337@gmail.com>
2024-10-09 18:54:11 +0800

    finished the advs section

--- src/ts/modules/animation.ts
+++ src/ts/modules/animation.ts
@@ -4,7 +4,11 @@ export default class AnimationHandler {
     openBurgerMenuBtn: HTMLElement | null;
     closeBurgerMenuBtn: HTMLElement | null;
 
-
+    carouselParent: NodeListOf<HTMLElement> | null;
+    carouselViewportSelector: string;
+    carouselControlsSelector: string;
+    carouselBtnSelector: string;
+    carouselCurrentBtnClass: string;
 
     constructor() {
         // Burger Menu
@@ -12,10 +16,67 @@ export default class AnimationHandler {
         this.openBurgerMenuBtn = document.querySelector('.header__popup-close');
         this.closeBurgerMenuBtn = document.querySelector('.header__popup-open');
         this.burgerMenuOpenClass = 'header__nav--open';
+
+        // Carousels
+        this.carouselParent = document.querySelectorAll('.carousel-parent');
+        this.carouselViewportSelector = '.carousel-viewport';
+        this.carouselControlsSelector = '.carousel-controls';
+        this.carouselBtnSelector = '.slide';
+        this.carouselCurrentBtnClass = 'slide--selected';
     }
 
     init() {
         this.toggleBurgerMenu();
+        this.initCarousel();
+    }
+
+    initCarousel() {
+        this.carouselParent?.forEach(parent => {
+            this.setupCarousel(parent);
+        })
+    }
+
+    setupCarousel(parent: HTMLElement | null) {
+        if (!parent) return
+
+        const viewport: HTMLElement | null = parent.querySelector(this.carouselViewportSelector);
+        const controls: HTMLElement | null = parent.querySelector(this.carouselControlsSelector);
+        if (!controls) return
+        const btns: NodeListOf<HTMLElement> | null = controls.querySelectorAll(this.carouselBtnSelector);
+
+        if (!btns) return
+
+        btns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                const currentOrder = btn.getAttribute('data-item');
+
+                if (currentOrder) {
+                    const offset = Number(currentOrder) - 1;
+
+                    if (!viewport) return
+                    const firstChildRect = (viewport.firstElementChild as HTMLElement).getBoundingClientRect();
+                    const styles = window.getComputedStyle(viewport);
+                    const gapValue = parseFloat(styles.gap || styles.columnGap) || 0; 
+                    const totalWidth = firstChildRect.width + gapValue;
+                    viewport.style.transform = `translateX(-${totalWidth * offset}px)`;
+                    
+                    this.reassignCurrentClass(btns, btn);
+
+                }
+            })
+        })
+
+    }
+
+    reassignCurrentClass(btns: NodeListOf<HTMLElement> | null, currentBtn: HTMLElement | null) {
+        if (!btns) return
+
+        btns.forEach(btn => {
+            if (btn !== currentBtn) {
+                btn.classList.remove(this.carouselCurrentBtnClass);
+            }
+            currentBtn?.classList.add(this.carouselCurrentBtnClass);
+        })
     }
 
     toggleBurgerMenu() {
